41413c43df948bb5bf000edc3399770440d2cedd
NIHAL-TP <nihaltp101@gmail.com>
2025-04-30 21:09:26 +0530

    updated sonarqube code analysis and code quality check

--- vars/SonarQubeAnalysis.groovy
+++ vars/SonarQubeAnalysis.groovy
@@ -2,7 +2,7 @@ def call(String sonarQubeApi,String projectName,String projectKey)
 {
     withSonarQubeEnv("${sonarQubeApi}")
     {
-        sh """$SONAR_HOME/bin/sonar-scanner
+        sh """ $SONAR_HOME/bin/sonar-scanner
         -Dsonar.projectName=${projectName}
         -Dsonar.projectKey=${projectKey} -X """
     }
