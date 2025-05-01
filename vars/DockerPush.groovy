8a096bf6bc9195022a2301e8dbf01b5bec45b602
NIHAL-TP <nihaltp101@gmail.com>
2025-05-01 13:34:48 +0530

    docker push

--- vars/DockerPush.groovy
+++ vars/DockerPush.groovy
@@ -1,6 +1,5 @@
 def call(image,tag){
-    withCredentials([usernamePassword(credentialsId:"${Docker-
-    cred}",passwordVariable:"${dockerHubPass}",usernameVariable:"${dockerHubUser}")])
+    withCredentials([usernamePassword(credentialsId:"${Docker-cred}",passwordVariable:"${dockerHubPass}",usernameVariable:"${dockerHubUser}")])
     {
         sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
         sh "docker push ${dockerHubUser}/${image}:${tag}"
