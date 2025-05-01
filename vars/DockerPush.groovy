232f48dad99baffc9f8e06cbd3793f03d2917e64
NIHAL-TP <nihaltp101@gmail.com>
2025-05-01 14:22:55 +0530

    docker push jenkins secrets  pass to shared library fix

--- vars/DockerPush.groovy
+++ vars/DockerPush.groovy
@@ -1,7 +1,7 @@
 def call(String image,String tag){
     withCredentials([usernamePassword(credentialsId:"Docker-cred",passwordVariable:"dockerHubPass",usernameVariable:"dockerHubUser")])
     {
-        sh 'echo "$dockerHubPass" | docker login -u "$dockerHubUser" -p --password-stdin'
+        sh 'echo "$dockerHubPass" | docker login -u "$dockerHubUser" -p --password-stdin docker.io'
         sh "docker push ${dockerHubUser}/${image}:${tag}"
     }
 }
