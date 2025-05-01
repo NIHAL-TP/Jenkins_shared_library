590f13a0aca951fe7676f2c6e93c73d9852ab87e
NIHAL-TP <nihaltp101@gmail.com>
2025-05-01 15:24:07 +0530

    working docker push jenkins secrets  pass to shared library fix/changes in docker push,string interpolation cannot be used with secrets

--- vars/DockerPush.groovy
+++ vars/DockerPush.groovy
@@ -1,7 +1,9 @@
 def call(String image,String tag){
     withCredentials([usernamePassword(credentialsId:"Docker-cred",passwordVariable:"dockerHubPass",usernameVariable:"dockerHubUser")])
     {
+        sh "echo 'image inside double q' ${image}"
         sh '''
+        echo "image inside single q" ${image}
         echo "$dockerHubPass" | docker login -u "$dockerHubUser" --password-stdin docker.io
         docker push "$dockerHubUser/${image}:${tag}"
         docker logout docker.io
